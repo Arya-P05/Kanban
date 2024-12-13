fb49a0b4dc0bafba50686912b47e1ebf392c43cb
Arya <jackrack040@gmail.com>
2024-12-12 20:32:55 -0500

    board basics

--- kanban/app/page.tsx
+++ kanban/app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Kanban = () => {
-  return <div className="h-screen w-full bg-neutral-900 text-neutral-50"></div>;
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      <Board />
+    </div>
+  );
+};
+
+const Board = () => {
+  return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>;
 };
 
 export default Kanban;
