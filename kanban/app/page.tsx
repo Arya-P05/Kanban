394f3d1b5e46d0c79c57169e508801985d02ecdb
Arya <jackrack040@gmail.com>
2024-12-12 20:40:16 -0500

    sample columns

--- kanban/app/page.tsx
+++ kanban/app/page.tsx
@@ -12,7 +12,38 @@ const Kanban = () => {
 const Board = () => {
   const [cards, setCards] = useState([]);
 
-  return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>;
+  return (
+    <div className="flex h-full w-full gap-3 overflow-scroll p-12">
+      <Column
+        title="Backlog"
+        column="backlog"
+        headingColour="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="TODO"
+        column="todo"
+        headingColour="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="In progress"
+        column="doing"
+        headingColour="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Complete"
+        column="done"
+        headingColour="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
+    </div>
+  );
 };
 
 const Column = ({ title, headingColour, column, cards, setCards }) => {
@@ -20,3 +51,33 @@ const Column = ({ title, headingColour, column, cards, setCards }) => {
 };
 
 export default Kanban;
+
+const DEFAULT_CARDS = [
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+  { title: "SOX compliance checklist", id: "2", column: "backlog" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+  { title: "Document Notifications service", id: "4", column: "backlog" },
+  // TODO
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  // DOING
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "doing",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+  // DONE
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
+];
