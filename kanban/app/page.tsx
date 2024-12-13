8815f19a9c44b14b421d84447abd53e804a992ac
Arya <jackrack040@gmail.com>
2024-12-13 02:00:51 -0500

    backlog

--- kanban/app/page.tsx
+++ kanban/app/page.tsx
@@ -21,7 +21,7 @@ const Board = () => {
       <div className="flex gap-3 p-12 overflow-x-scroll">
         <Column
           title="3 Points"
-          column="3pt"
+          column="backlog"
           headingColor="text-green-500"
           cards={cards}
           setCards={setCards}
@@ -318,9 +318,33 @@ const AddCard = ({ column, setCards }) => {
 };
 
 const DEFAULT_TEAMS = [
-  { title: "Team Yash", id: "1", column: "3pt" },
-  { title: "Team Preet", id: "2", column: "3pt" },
-  { title: "Team Rushi", id: "3", column: "3pt" },
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+  { title: "SOX compliance checklist", id: "2", column: "backlog" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+  { title: "Document Notifications service", id: "4", column: "backlog" },
+  // TODO
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  // DOING
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "doing",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+  // DONE
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
 ];
 
 export default Kanban;
