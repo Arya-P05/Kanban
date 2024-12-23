16d534329ea7da07d53475afa903813f1d00282c
Arya <jackrack040@gmail.com>
2024-12-23 16:29:27 -0500

    no scrollbar

--- kanban/app/page.tsx
+++ kanban/app/page.tsx
@@ -4,11 +4,14 @@ import React, { useEffect, useState, useMemo } from "react";
 import { FiPlus, FiTrash } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { FaFire } from "react-icons/fa";
+import styles from "./kanban.module.css";
 
 const Kanban = () => {
   return (
-    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
-      <Board />
+    <div className={styles.noScrollbar}>
+      <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+        <Board />
+      </div>
     </div>
   );
 };
