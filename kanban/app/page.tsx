40fb13228edf6f445020574c2c9a1f8167824830
Arya <jackrack040@gmail.com>
2024-12-23 22:47:12 -0500

    remove default tasks

--- kanban/app/page.tsx
+++ kanban/app/page.tsx
@@ -33,7 +33,7 @@ const Board = () => {
 
   useEffect(() => {
     const cardData = localStorage.getItem("kanban");
-    setCards(cardData ? JSON.parse(cardData) : DEFAULT_TASKS);
+    setCards(cardData ? JSON.parse(cardData) : []);
     setHasChecked(true);
   }, []);
 
@@ -399,33 +399,33 @@ const AddCard = ({
   );
 };
 
-const DEFAULT_TASKS = [
-  // BACKLOG
-  // { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
-  // { title: "SOX compliance checklist", id: "2", column: "backlog" },
-  // { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
-  // { title: "Document Notifications service", id: "4", column: "backlog" },
-  // TODO
-  // {
-  //   title: "Research DB options for new microservice",
-  //   id: "5",
-  //   column: "todo",
-  // },
-  // { title: "Postmortem for outage", id: "6", column: "todo" },
-  // { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
-  // DOING
-  // {
-  //   title: "Refactor context providers to use Zustand",
-  //   id: "8",
-  //   column: "doing",
-  // },
-  // { title: "Add logging to daily CRON", id: "9", column: "doing" },
-  // DONE
-  // {
-  //   title: "Set up DD dashboards for Lambda listener",
-  //   id: "10",
-  //   column: "done",
-  // },
-];
+// const DEFAULT_TASKS = [
+//   // BACKLOG
+//   // { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+//   // { title: "SOX compliance checklist", id: "2", column: "backlog" },
+//   // { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+//   // { title: "Document Notifications service", id: "4", column: "backlog" },
+//   // TODO
+//   // {
+//   //   title: "Research DB options for new microservice",
+//   //   id: "5",
+//   //   column: "todo",
+//   // },
+//   // { title: "Postmortem for outage", id: "6", column: "todo" },
+//   // { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+//   // DOING
+//   // {
+//   //   title: "Refactor context providers to use Zustand",
+//   //   id: "8",
+//   //   column: "doing",
+//   // },
+//   // { title: "Add logging to daily CRON", id: "9", column: "doing" },
+//   // DONE
+//   // {
+//   //   title: "Set up DD dashboards for Lambda listener",
+//   //   id: "10",
+//   //   column: "done",
+//   // },
+// ];
 
 export default Kanban;
